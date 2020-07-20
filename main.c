e19682f85ad23c1a6f34a23d11d952c137a7286b
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-07-19 23:13:46 -0400

    comments for threads

--- main.c
+++ main.c
@@ -5,12 +5,21 @@
 #include "lfsr113.h" 
 
 void client(void *arg){
+	// wait a random period of time and then send a message to one of the two queues, alternating 
+	// between them. Repeats indefinitely
 }
 
 void server(void *arg){
+	// start by determing which queue it's watching then wait for a random period of time before 
+	// retrieving a message from its queue. Repeats indefinitely
 }
 
 void monitor(void *arg){
+	// runs once a second and should print the elasped time in seconds as well as other info 
+	
+	// total messages sent successfully 
+	// total messages received 
+	// total overflows (when osMessageQueuePut returns osErrorResource
 }
 
 int main(){
@@ -24,6 +33,7 @@ int main(){
 	osThreadNew(server, NULL, NULL); 
 	osThreadNew(server, NULL, NULL); 
 	osThreadNew(monitor, NULL, NULL); 
+	
 	// start the Kernel
 	osKernelStart(); 
 }
